e215c3cc6127d25c2714a983c7b189a18589e80f
sahebbali <sahebbali253@gmail.comm>
2023-08-16 11:52:36 +0600

    add some one

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
 // import './App.css'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Singup from './pages/sing-up/singup';
+import SingUp from './pages/singup/Singup.jsx';
 import Faq from './pages/FAQ/Faq';
 
 function App() {
@@ -13,9 +13,8 @@ function App() {
       <Routes>
         <Route path="/"  element={<Home/>} />
         <Route path="/login"  element={<Login/>} />
-        <Route path="/signup"  element={<Singup/>} />
+        <Route path="/signup"  element={<SingUp/>} />
          <Route path="/faq"  element={<Faq/>} />
-
       </Routes>
     </Router>
    
